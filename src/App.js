637609121292f9e904768495efbf0a39f6d059ae
Ankit Kumar <60535576+ankitkumar0021@users.noreply.github.com>
2023-07-01 04:30:18 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import CounterExercise from "./Components/CounterExercise";
+import PrintingList from "./Components/PrintingList";
 
 export default function App() {
   return (
     <div className="App">
-      <CounterExercise />
+      {/* {/* <CounterExercise /> */} */}
+      <PrintingList />
     </div>
   );
 }
